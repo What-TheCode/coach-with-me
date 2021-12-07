97d35de610c612f584745a037fed0b9e211cb78f
Alexander <alexandervanw@gmail.com>
2021-12-07 09:21:25 +0100

    Initial commit

---
+++ src/main/java/com/example/coachwithme/CoachWithMeApplication.java
@@ -0,0 +1,13 @@
+package com.example.coachwithme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachWithMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoachWithMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coachwithme/CoachWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coachwithme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachWithMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
