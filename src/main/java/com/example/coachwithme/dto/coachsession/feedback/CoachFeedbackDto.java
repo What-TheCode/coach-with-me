d40385277e1c73600234c73cf9987bd43d8c77c3
Thomas Opheide <thomasopheide@hotmail.com>
2021-12-20 18:55:28 +0100

    Added some more validation on the feedback Dtos

--- src/main/java/com/example/coachwithme/dto/coachsession/feedback/CoachFeedbackDto.java
+++ src/main/java/com/example/coachwithme/dto/coachsession/feedback/CoachFeedbackDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,10 +17,15 @@ public class CoachFeedbackDto {
 
     private int id;
 
+    @Min(value = 1, message = "Value cannot be lower than 1.")
+    @Max(value = 5, message = "Value cannot be higher than 5.")
     private int coacheePreparedRating;
 
+    @Min(value = 1, message = "Value cannot be lower than 1.")
+    @Max(value = 5, message = "Value cannot be higher than 5.")
     private int coacheeProgressRating;
 
+    @NotBlank
     private String message;
 
 }
--- src/main/java/com/example/coachwithme/dto/coachsession/feedback/CoacheeFeedbackDto.java
+++ src/main/java/com/example/coachwithme/dto/coachsession/feedback/CoacheeFeedbackDto.java
@@ -6,6 +6,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 @Builder
 @AllArgsConstructor
@@ -15,10 +18,15 @@ public class CoacheeFeedbackDto {
 
     private int id;
 
+    @Min(value = 1, message = "Value cannot be lower than 1.")
+    @Max(value = 5, message = "Value cannot be higher than 5.")
     private int coachExplanationRating;
 
+    @Min(value = 1, message = "Value cannot be lower than 1.")
+    @Max(value = 5, message = "Value cannot be higher than 5.")
     private int coachSessionUsefullRating;
 
+    @NotBlank
     private String message;
 
 }
